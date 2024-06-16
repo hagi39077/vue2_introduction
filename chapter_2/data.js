38cbf2a884794215365bec24b02b195e2f783367
hagi-3 <hagi39077@gmail.com>
2024-06-16 23:01:03 +0900

    $watchメソッドの追加

--- chapter_2/data.js
+++ chapter_2/data.js
@@ -25,3 +25,16 @@ const vm = new Vue({
 
 // コンソール上で確認するためにwindowオブジェクトに代入
 window.vm = vm;
+
+// watchによる監視を追加
+// 値の変更を開始しているので、トリガーとなってビューの再描画・DOM要素の更新が行われる。
+vm.$watch(
+  function () {
+    // 鉛筆の個数
+    return this.items.quantity;
+  },
+  function (quantity) {
+    // このコールバックは、鉛筆の購入個数が変更されたら呼ばれる
+    console.log(`quantity: ${quantity}`);
+  }
+);
