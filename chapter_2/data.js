c24c558e5d9f6d30f4bd718fc53e911561d46062
hagi-3 <hagi39077@gmail.com>
2024-06-22 21:23:55 +0900

    computed（算出プロパティ）で実装 Mustache記法でも動作するが、computedに記述することで保守性があがる

--- chapter_2/data.js
+++ chapter_2/data.js
@@ -2,17 +2,17 @@ const items = [
   {
     name: "鉛筆",
     price: 300,
-    quantity: 0,
+    quantity: 2,
   },
   {
     name: "ノート",
     price: 400,
-    quantity: 0,
+    quantity: 1,
   },
   {
     name: "消しゴム",
     price: 500,
-    quantity: 0,
+    quantity: 3,
   },
 ];
 const vm = new Vue({
@@ -20,6 +20,14 @@ const vm = new Vue({
   data: {
     // dataプロパティ
     items: items,
+  },
+  computed: {
+    totalPrice: function () {
+      // this経由でインスタンス内のデータにアクセス
+      return this.items.reduce(function (sum, item) {
+        return sum + item.price * item.quantity;
+      }, 0);
+    },
   },
   filters: {
     numberWithDelimiter: function (value) {
